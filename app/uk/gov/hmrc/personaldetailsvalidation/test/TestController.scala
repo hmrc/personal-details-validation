5a2aae8c9b90d70f1637f0144a551d0faddb2ae1
Steve Edmans <steve.edmans@digital.hmrc.gov.uk>
2018-08-02 12:06:45 +0100

    AIV-154: Added test only endpoint to insert test data

---
+++ app/uk/gov/hmrc/personaldetailsvalidation/test/TestController.scala
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.personaldetailsvalidation.test
+
+import java.time.LocalDate
+import java.util.UUID
+import javax.inject.{Inject, Singleton}
+
+import play.api.libs.json.{Format, Json}
+import play.api.mvc.Action
+import uk.gov.hmrc.domain.Nino
+import uk.gov.hmrc.personaldetailsvalidation.FuturedPersonalDetailsValidationRepository
+import uk.gov.hmrc.personaldetailsvalidation.model.{PersonalDetailsWithNino, SuccessfulPersonalDetailsValidation, ValidationId}
+import uk.gov.hmrc.play.bootstrap.controller.BaseController
+import uk.gov.hmrc.play.json.JsonValidation
+import uk.gov.hmrc.play.http.logging.MdcLoggingExecutionContext._
+
+import scala.concurrent.Future
+
+@Singleton
+class TestController @Inject()(personalDetailsValidationRepository: FuturedPersonalDetailsValidationRepository)
+  extends BaseController
+    with JsonValidation {
+
+  def upsertTestValidation = Action.async(parse.json) { implicit request =>
+    withJsonBody[PersonalDetailsTestValidationData] { personalDetails =>
+      val personData = PersonalDetailsWithNino(firstName = "NA - Test Data", lastName = "NA - Test Data", dateOfBirth = LocalDate.now(), nino = personalDetails.nino)
+      val validationData = SuccessfulPersonalDetailsValidation(ValidationId(personalDetails.validationId), personData)
+      personalDetailsValidationRepository.create(validationData)
+      Future.successful(Created)
+    }
+  }
+
+  implicit val prvtdFormat : Format[PersonalDetailsTestValidationData] = Json.format[PersonalDetailsTestValidationData]
+}
+
+case class PersonalDetailsTestValidationData(validationId : UUID, nino: Nino)
