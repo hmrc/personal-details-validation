6bac81befc85dac5f65f97678bec24133217b1e5
ramesh <ramesh.sukka@gmail.com>
2022-06-22 14:18:39 +0100

    VER-2153- added more logs

--- app/uk/gov/hmrc/personaldetailsvalidation/matching/MatchingConnector.scala
+++ app/uk/gov/hmrc/personaldetailsvalidation/matching/MatchingConnector.scala
@@ -73,10 +73,9 @@ class MatchingConnectorImpl @Inject()(httpClient: HttpClient,
     override def read(method: String, url: String, response: HttpResponse): Either[Exception, MatchResult] = response.status match {
       case OK => Right(MatchSuccessful(response.json.as[PersonalDetails]))
       case UNAUTHORIZED => Right(MatchFailed((response.json \ "errors").as[String]))
-      case other => {
+      case other =>
         logger.warn(s"VER-2153 error in matchingResultHttpReads : " + other)
-        Left(new BadGatewayException(s"Unexpected response from $method $url with status: '$other' and body: ${response.body}"))
-      }
+        throw new BadGatewayException(s"Unexpected response from $method $url with status: '$other' and body: ${response.body}")
     }
   }
 
